e2c2e39e9f4f620eab1c984fd4d4a6f0e98ba701
mkaa <20796385+thomasbad@users.noreply.github.com>
2023-02-27 16:46:05 +0800

    Added About and Help Button for homework requirement

--- app/src/main/java/com/mka/ipcalculator/MainActivity.java
+++ app/src/main/java/com/mka/ipcalculator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mka.ipcalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     //Define variable type
     EditText ipText, subnetText;
     TextView resultText;
-    Button calculateButton, resetButton;
+    Button calculateButton, resetButton, aboutButton, helpButton;
 
     //Assign variable with view elements
     @Override
@@ -26,6 +27,22 @@ public class MainActivity extends AppCompatActivity {
         resultText = findViewById(R.id.result_text_view);
         calculateButton = findViewById(R.id.calculate_button);
         resetButton = findViewById(R.id.reset_button);
+        aboutButton = findViewById(R.id.about_button);
+        helpButton = findViewById(R.id.help_button);
+
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AboutActivity.class));
+            }
+        });
+
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HelpActivity.class));
+            }
+        });
 
 
     //Calculate Button function
